7385f97297f70b4f25a52c93e894ff56eec34b2d
Renata Silva Santos <natyybeky2017@gmail.com>
2026-09-16 14:25:31 +0200

    feat: start exercises

--- 1-variables-and-data-types.py
+++ 1-variables-and-data-types.py
@@ -9,7 +9,8 @@ Crea una variable llamada "mensaje".
 Asígnale el valor "¡Hola, Mundo!". 
 Imprime el valor de la variable en la consola.
 """
-# Escribe tu código aquí
+mensaje = "Hola Mundo"
+print(mensaje)
 
 """
 --- Ejercicio 2 Variables---
@@ -18,7 +19,9 @@ Reasígnale el valor "Hello world!".
 Imprime el valor de la variable en la consola.
 Escribe en un comentario de línea lo que sucede.
 """
-# Escribe tu código aquí
+mensaje = "Hello World"
+print ( mensaje)
+""" imprime la segunda variable"""
 
 """
 --- Ejercicio 3 Tipos de datos---
@@ -26,5 +29,20 @@ Crea variables para cada uno de los siguientes tipos de datos y colecciones: str
 bool, list, tuple, dicctionary and set. 
 Imprime cada variable y el tipo de dato o colección que almacena en la consola.
 """
-# Escribe tu código aquí
+a = Hola # string
+b = 10 # int
+c = 1.5 # float
+d = true # bool
+e = [1,2] # list
+f = (1,2) # tuple
+g = {"x":1} # dicctionary
+h = {1, 2} # set
 
+print (a, type (a) )
+print (b, type (b))
+print (c, type (c))
+print (d, type (d))
+print (e, type (e))
+print (f, type (f))
+print (g, type (g))
+print (h, type (h))
